b2b58adc68d81e7c9ab44b6dc91dbfd33db519f8
Faiz KHAN <fkhan24@lab6-1.CS.McGill.CA>
2012-11-17 00:35:20 -0500

    Socket reading and writing works

--- src/trading/Prices.java
+++ src/trading/Prices.java
@@ -3,10 +3,11 @@ package trading;
 public class Prices
 {
     private int MAX_SECONDS = 32400; 
-    private float[] prices = new float[MAX_SECONDS];
+    private float[] prices;
     
     public static Prices GetPrices()
     {
+        if(Prices.pricesInstance == null) Prices.pricesInstance = new Prices();
         return pricesInstance;
     }
     
@@ -14,16 +15,27 @@ public class Prices
     private static Prices pricesInstance = new Prices(); 
     private Prices()
     {
+        prices = new float[MAX_SECONDS];
+        for(int i=0; i<prices.length; ++i)
+        {
+            prices[i] = -1f;
+        }
     }
     
     public float GetPrice(int tick)
     {
         return prices[tick];
-    }
-    
+    }  
     public void SetPrice(int tick, float value)
     {
         prices[tick] = value; 
     }
 
+    public void printPrices()
+    {
+       for(int i = 0; i<prices.length; ++i)
+       {
+           System.out.println("The price at tick " + i + " is " + prices[i]);
+       }
+    }
 }
