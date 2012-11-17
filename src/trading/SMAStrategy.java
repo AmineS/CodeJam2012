900078d1dfc53125bcc4be6d0e6d3cd17f5d1f0f
Pepe <aminesahibi@gmail.com>
2012-11-17 00:04:29 -0500

    Added the implementation of the SMA strategy

--- src/trading/SMAStrategy.java
+++ src/trading/SMAStrategy.java
@@ -1,6 +1,99 @@
 package trading;
 
-public class SMAStrategy extends AStrategy
+public class SMAStrategy extends AStrategy implements Runnable
 {
+    
+    @Override
+    public void run()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    @Override 
+    public void runStrategy(Prices prices)
+    {
+        computeSlowSMA(prices);
+        computeFastSMA(prices);
+        
+        if(currentTick > 1)
+        {
+            boolean currentFasterThenSlower = fastSMAValues[currentTick] > slowSMAValues[currentTick];
+            if(currentFasterThenSlower != fasterThenSlower)
+            {
+                crossover(currentFasterThenSlower);
+            }
+        }
+        else
+        {
+            fasterThenSlower = fastSMAValues[currentTick] > slowSMAValues[currentTick];
+        }
+        currentTick++;
+    }
+    
+    public float computeSlowSMA(Prices prices)
+    {
+        if (currentTick <= 5)
+        {
+            for(int i = 0; i < currentTick; i++)
+            {
+                slowSMAValues[currentTick] += prices.GetPrice(currentTick);
+            }
+            slowSMAValues[currentTick] /= currentTick;
+        }
+        else
+        {
+            slowSMAValues[currentTick] = slowSMAValues[currentTick - 1] + (prices.GetPrice(currentTick) - prices.GetPrice(currentTick - 5)) / 5;
+        }
+        
+        return slowSMAValues[currentTick];
+    }
+    
+    public float computeFastSMA(Prices prices)
+    {
+        if (currentTick <= 20)
+        {
+            for(int i = 0; i < currentTick; i++)
+            {
+                fastSMAValues[currentTick] += prices.GetPrice(currentTick);
+            }
+            fastSMAValues[currentTick] /= currentTick;
+        }
+        else
+        {
+            fastSMAValues[currentTick] = fastSMAValues[currentTick - 1]  + (prices.GetPrice(currentTick) - prices.GetPrice(currentTick - 20)) / 20;
+        }
+        
+        return fastSMAValues[currentTick];
+    }
+    
+    @Override
+    public char crossover()
+    {
+     // TODO Auto-generated method stub
+        return 'A';
+    }
+        
+    @Override
+    public int getTick()
+    {
+        return currentTick;
+    }
+    
+    public float[] getSlow()
+    {
+        return slowSMAValues;
+    }
+    
+    public float[] getFast(Prices prices)
+    {
+        return fastSMAValues;
+    }
+
+    
+    private int currentTick;
+    private boolean fasterThenSlower;
+    public static float[] slowSMAValues;
+    public static float[] fastSMAValues;
 
 }
